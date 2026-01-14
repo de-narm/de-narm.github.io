ec6c4fbdef4e5752cc45f06890e74425b1cea798
De Narm <s3152806@msx.tu-dresden.de>
2026-01-14 19:55:28 +0100

    inital commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import './layout.css';
+	import logo from '$lib/assets/logo.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={logo} />
+</svelte:head>
+
+{#snippet navButton(text: string, isOpen: boolean)}
+  <div class="flex align-center px-1 {isOpen ? 'border-b-2 border-white' : ''}">
+    {text} 
+  </div>
+{/snippet}
+
+<nav class="bg-black text-white flex p-2 my-2 mx-10 rounded-xl font-bold gap-10 justify-center shadow-lg shadow-gray-500/20">
+  {@render navButton("Home", true)}
+  {@render navButton("Showcase", false)}
+  {@render navButton("Pricing", false)}
+</nav>
+
+{@render children()}
+
+<!-- background grid -->
+<div class="bg-gray-light w-full h-full absolute -z-2 inset-0" />
+<div class="absolute -z-1 inset-0 top-[31px] left-[31px] h-full w-full
+            bg-[radial-gradient(circle,#232525ff_5px,transparent_5px)]
+            bg-[size:60px_60px]" />
+<div class="absolute -z-1 inset-0 h-full w-full
+            bg-[linear-gradient(to_right,#232525ff_2px,transparent_2px),linear-gradient(to_bottom,#232525ff_2px,transparent_2px)]
+            bg-[size:60px_60px]" />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.svg';
+</script>
+
+{#snippet transition(startColor: string, endColor: string, rotate: boolean = false)}
+  <div class="w-full h-[50px] flex bg-{startColor} {rotate ? 'rotate-180' : ''}">
+    <div class="h-full w-full "></div>
+    <div class="h-full min-w-[600px] max-w-[600px] bg-{endColor}"
+         style="clip-path: path('M 0 50 Q 150 50 300 25 Q 450 0 600 0 L 600 50');"
+    ></div>
+    <div class="h-full w-full bg-{endColor}"></div>
+  </div>
+{/snippet}
+
+{#snippet colorBlock(color: string)}
+  {@render transition("transparent", color)}
+  <div class="w-full h-[200px] bg-{color}">
+  </div>
+  {@render transition("transparent", color, true)}
+{/snippet}
+
+<div class="flex justify-center">
+  <img class="w-1/3 m-10" src={logo} />
+</div>
+
+{@render colorBlock("purple-logo")}
+
+<div class="flex justify-center">
+  <img class="w-1/3 m-10" src={logo} />
+</div>
+
+{@render colorBlock("black")}
+
+<div class="flex justify-center">
+  <img class="w-1/3 m-10" src={logo} />
+</div>
+
