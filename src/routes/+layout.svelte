91aa8852b2b2ad738cd8cf866809101b45a6d098
De Narm <s3152806@msx.tu-dresden.de>
2026-01-19 22:32:17 +0100

    example frame

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import github from '$lib/assets/github.svg';
 
 	let { children } = $props();
+
+  let scrollContainerHeight: number = $state();
 </script>
 
 <svelte:head>
@@ -17,41 +19,67 @@
   </div>
 {/snippet}
 
-<!-- Navigation -->
-<div class="bg-black/93 text-white w-screen h-fit flex justify-center">
-  <div class="w-[980px] p-5 z-1">
-    <nav class="flex justify-between my-5">
-      <div class="flex gap-1 items-center">
-        <img class="w-[30px]" src={logo}>
-        <span class="font-extrabold pl-1 pr-3 text-2xl">Explorama</span>
-        {@render navButton("Home", true)}
-        {@render navButton("Showcase", false)}
-        {@render navButton("Pricing", false)}
-      </div>
-      <img class="w-[30px] opacity-70" src="{github}">
-    </nav>
-  </div>
-</div>
-
-<div class="bg-black/93 text-white w-screen h-screen flex relative
-            justify-center items-center overflow-y-auto flex-col">
-  <!-- Main Container -->
-  <div class="w-[980px] p-5 z-1 h-full">
-    {@render children()}
-  </div>
-  
-  <!-- Grid Container -->
-  <div class="absolute z-0 top-0 bottom-20 w-full
-              max-w-[1380px] opacity-80">
-    <div class="absolute inset-0 h-full w-full flex
-                mask-y-from-95% mask-x-from-80% overflow-hidden">
-      <div class="absolute top-[31px] left-[31px] h-full w-full
-                  bg-[radial-gradient(circle,#232525ff_5px,transparent_5px)]
-                  bg-[size:60px_60px]" />
-      <div class="absolute h-full w-full
-                  bg-[linear-gradient(to_right,#232525ff_2px,transparent_2px),linear-gradient(to_bottom,#232525ff_2px,transparent_2px)]
-                  bg-[size:60px_60px]" />
+<div class="w-screen h-screen overflow-hidden bg-black/93 text-white
+            flex flex-col">
+  <!-- Navigation -->
+  <div class="w-full h-fit flex justify-center">
+    <div class="w-[980px] p-5 z-1">
+      <nav class="flex justify-between my-5">
+        <div class="flex gap-1 items-center">
+          <img class="w-[30px]" src={logo}>
+          <span class="font-extrabold pl-1 pr-3 text-2xl">Explorama</span>
+          {@render navButton("Home", true)}
+          {@render navButton("Showcase", false)}
+          {@render navButton("Pricing", false)}
+        </div>
+        <img class="w-[30px] opacity-70" src="{github}">
+      </nav>
     </div>
   </div>
+  
+  <!-- Main Container -->
+  <div class="w-full h-full flex relative items-center 
+              overflow-y-auto flex-col">
+    <!-- Page -->
+    <div bind:clientHeight={scrollContainerHeight} class="w-[980px] p-5 z-1">
+      {@render children()}
+    </div>
+    
+    <!-- Footer -->
+    <div class="w-full z-1 flex justify-center text-white/80">
+      <div class="bg-black/95 w-[980px] p-10 z-1 flex rounded-t-md">
+        <div class="w-full flex flex-col">
+          <span>Impressum</span>
+          <span>Vision</span>
+          <span>Roadmap</span>
+        </div>
+        <div class="w-full flex flex-col">
+          <span>Pricing</span>
+          <span>Contact</span>
+        </div>
+        <div class="w-full flex flex-col">
+          <span>Github</span>
+          <span>LinkedIn</span>
+          <span>Discord</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- Grid Container -->
+    <div class="absolute z-0 top-0 w-full
+                max-w-[1180px] opacity-80"
+         style="height: {scrollContainerHeight ?? 0}px;">
+      <div class="absolute inset-0 h-full w-full flex
+                  mask-y-from-95% mask-x-from-80% overflow-hidden">
+        <div class="absolute top-[31px] left-[31px] h-full w-full
+                    bg-[radial-gradient(circle,#232525ff_5px,transparent_5px)]
+                    bg-[size:60px_60px]" />
+        <div class="absolute h-full w-full
+                    bg-[linear-gradient(to_right,#232525ff_2px,transparent_2px),linear-gradient(to_bottom,#232525ff_2px,transparent_2px)]
+                    bg-[size:60px_60px]" />
+      </div>
+    </div>
+  </div>
+
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <!-- Main Block -->
-<div class="w-full py-20 flex-col flex justify-center items-center gap-6 
+<div class="w-full pt-20 flex-col flex justify-center items-center gap-6 
             relative">
   <!-- Intro Text -->
   <div class="w-[400px] text-center text-5xl font-semibold z-1">
@@ -23,6 +23,40 @@
   <div class="mt-20 p-10">
     <div class="bg-zinc-950 p-4 border border-zinc-600 rounded-3xl">
       <img src={screenshot} class="rounded-2xl">
+    </div>
+  </div>
+
+  <div class="mt-20 px-10 relative w-full flex items-center">
+    <!-- Example frame -->
+    <div class="w-[650px] h-[400px] bg-frame-bg rounded-xl overflow-hidden 
+                flex flex-col">
+      <!-- Header -->
+      <div class="w-full h-10 flex relative">
+        <div class="w-2/3 h-full bg-frame-gray flex justify-end">
+          <div class="h-full w-[200px] bg-frame-fill"
+               style="clip-path: path('M 0 40 Q 50 40 100 20 Q 150 0 200 0 L 200 40');"
+          >
+          </div>
+        </div>
+        <div class="w-1/3 h-full bg-frame-fill">
+        </div>
+        <div class="absolute left-10 top-2">
+          Some title
+        </div>
+      </div>
+
+      <!-- Content -->
+      <div class="w-full h-full border-y-3 border-frame-fill flex items-center 
+                  justify-center">
+                  Some pseudo interactive frame examples maybe?
+      </div>
+
+    </div>
+
+    <!-- Example note -->
+    <div class="absolute right-0 w-[350px] h-[200px] bg-zinc-400 rounded-4xl
+                flex items-center justify-center text-black">
+      Some Feature notes!
     </div>
   </div>
 
